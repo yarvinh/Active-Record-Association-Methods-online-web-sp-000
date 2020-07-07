2018c31c7d36a6be32d076e9a51c887cf5ed80b7
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-07 00:08:57 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,6 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
+    self.songs[0].genre
     #return the genre of the artist's first saved song
   end
 
