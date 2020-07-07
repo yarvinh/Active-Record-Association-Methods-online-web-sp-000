8396ded763ba6bc9a7103d1dc1b1fb35e1ca0739
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-07 00:09:53 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,6 +13,9 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
+    artists = []
+self.artists.each{|artist| artists << artist.name}
+artists
     # return an array of strings containing every musician's name
   end
 end
